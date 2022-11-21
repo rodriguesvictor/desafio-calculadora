0eccd33246f167c2bc6629b63acd21bdc54ce4d2
Victor Rodrigues <Victordarkiin@gmail.com>
2022-11-21 18:05:50 -0300

    Estilização do Input

---
+++ src/components/Input/styles.js
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { device } from '../../device';
+
+export const InpuntContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    border-radius: 22px;
+    margin-bottom: 30px;
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #cc99fae3;
+        border: none;
+
+        font-size: 24px;
+        font-weight: 400;
+
+        text-align: right;
+
+        padding: 0 152px;
+
+        color: #fff;
+
+        border-radius: 22px;
+
+        box-shadow: 0 0 30px rgba(0,0,0,.38);
+
+
+        @media only screen and ${device.laptop}{
+            padding: 0;
+            text-align: center;
+        }
+
+
+        @media only screen and ${device.laptopL}{
+            padding: 0 80px;
+        }
+        
+    }
+`
+
